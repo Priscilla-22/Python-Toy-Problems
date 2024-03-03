a830884fde1bfb46ce62950c6435cc8523ba2081
Priscilla-22 <priscillamw123@gmail.com>
2024-03-03 22:08:04 +0300

    feat:minimum moves to reach 10 bricks in each box

---
+++ lib/toy_problems.py
@@ -0,0 +1,16 @@
+class Bricks:
+
+    def solution(self, A):
+        moves = 0
+
+        for i in range(len(A)):
+            if A[i] != 10:
+                difference_in_current_value = 10 - A[i]
+                A[i] += difference_in_current_value
+                if i + 1 < len(A):
+                    A[i + 1] -= difference_in_current_value
+                    moves += difference_in_current_value
+                else:
+                    return -1
+
+        return moves
