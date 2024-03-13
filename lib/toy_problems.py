ba201cffaf2ed9b65f786c6b3cda1715e858fcd9
Priscilla-22 <priscillamw123@gmail.com>
2024-03-13 23:49:46 +0300

    chore: update the readme with important details

--- lib/toy_problems.py
+++ lib/toy_problems.py
@@ -1,36 +1,52 @@
 ########## Challenge 1 ##########
-def bricks_moves_solution(A):
-    moves = 0
+def solution(A):
+    total_bricks = sum(A)
+    target_bricks = len(A) * 10
 
-    for i in range(len(A)):
-        if A[i] != 10:
-            difference_in_current_value = 10 - A[i]
-            A[i] += difference_in_current_value
-            if i + 1 < len(A):
-                A[i + 1] -= difference_in_current_value
-                moves += difference_in_current_value
-            else:
-                return -1
+    if total_bricks != target_bricks:
+        return -1
+
+    moves = 0
+    diff = 0
+
+    for i in range(len(A) - 1):
+        diff = A[i] - 10
+        A[i] -= diff
+        A[i + 1] += diff
+        moves += abs(diff)
 
     return moves
 
 
+print(solution([7, 14, 10]))
+
+
 ########## Challenge 2 ##########
 def sum_of_intergers_solution(B):
+    digit_sums = {}
     max_sum = -1
-    for i in range(len(B)):
-        for j in range(i + 1, len(B)):
-            sum_i = sum(int(digit) for digit in str(B[i]))
-            sum_j = sum(int(digit) for digit in str(B[j]))
-            if sum_i == sum_j:
-                max_sum = max(max_sum, B[i] + B[j])
-    return max_sum
+
+    for num in B:
+        digit_sum = sum(int(digit) for digit in str(num))
+        if digit_sum in digit_sums:
+            max_sum = max(max_sum, digit_sums[digit_sum] + num)
+            digit_sums[digit_sum] = max(digit_sums[digit_sum], num)
+        else:
+            digit_sums[digit_sum] = num
+
+    return max_sum if max_sum != -1 else -1
 
 
-########## Challenge  ##########
+print(sum_of_intergers_solution([51, 71, 17, 42]))  # Expected output: 93
+
+
+########## Challenge 3 ##########
 def letter_occurrence_solution(N):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     letters = "".join([alphabet[i] for i in range(N // 2)]) * 2
     if N % 2 == 1:
         letters += alphabet[N // 2]
     return letters
+
+
+print(letter_occurrence_solution(30))
